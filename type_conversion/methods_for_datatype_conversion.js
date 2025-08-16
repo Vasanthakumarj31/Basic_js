1c42c75e531ce9044f3763a36d20de738b690e1d
Vasanth <vasanthvasanth4863@gmail.com>
2025-08-16 22:29:31 +0530

    Create methods_for_datatype_conversion.js

---
+++ type_conversion/methods_for_datatype_conversion.js
@@ -0,0 +1,17 @@
+//type conversion = change the datatype of the value to the another (strings,numbers,booleans)
+age = window.prompt("enter your age :");
+age = Number(age);
+age +=1;
+console.log(age,typeof age);
+
+let x;
+let y;
+let z;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x,typeof x);
+console.log(y,typeof y);
+console.log(z,typeof z);
