e1fe95838acb76bcc5f45b58bc4feef50ee56c78
Vasanth <vasanthvasanth4863@gmail.com>
2025-08-26 13:48:36 +0530

    Create math_object.js

---
+++ math_object.js
@@ -0,0 +1,22 @@
+//Math built in object that provides a 
+//collection of properties and methods
+let x = 3.4;
+let y = 2;
+let z;
+
+a=Math.round(x);
+b=Math.ceil(x);
+c=Math.floor(x);
+d=Math.trunc(x);
+e=Math.pow(x,y);
+f=Math.sqrt(x);
+g=Math.log(x);
+h=Math.sin(x);
+i=Math.cos(x);
+j=Math.tan(x);
+l=Math.abs(x);
+m=Math.sign(x);
+n=Math.max(x,y);
+o=Math.min(x,y);
+
+console.log(d);
