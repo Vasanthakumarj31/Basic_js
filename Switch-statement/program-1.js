d5f79176b4e390b0cb062106b7ac8a084f41d5f0
Vasanth <vasanthvasanth4863@gmail.com>
2025-09-19 19:01:01 +0530

    Create program-1.js

---
+++ Switch-statement/program-1.js
@@ -0,0 +1,28 @@
+//Switch = can be efficient replacement to many else if statement
+
+var day = 0
+switch(day){
+    case 1 :
+        console.log("It is monday");
+        break;
+    case 2 :
+        console.log("it is tuesday");
+        break;
+    case 3 :
+        console.log("It is wednesday");
+        break;
+    case 4 :
+        console.log("It is thursday");
+        break;
+    case 5 :
+        console.log("It is friday");
+        break;
+    case 6 : 
+        console.log("It is saturday");
+        break;
+    case 7 :
+        console.log("it is sunday");
+        break;
+    default:
+        console.log("invalid number")
+}
