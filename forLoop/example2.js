0d967f75b736b43fe9ca5edb23861994717826a4
Vasanth <vasanthvasanth4863@gmail.com>
2025-09-30 23:14:42 +0530

    Create example2.js

---
+++ forLoop/example2.js
@@ -0,0 +1,14 @@
+//for loop = repeat some code a LIMITED amount of times
+//continue and break keyword
+/*for(let i = 0;i<10;i++){
+    if(i === 8){
+        continue
+    }
+    console.log(i);
+}*/
+for(let i = 0;i<10;i++){
+    if(i === 8){
+        break
+    }
+    console.log(i);
+}
